695c39ccdd520e824c2480dfd966310147586d3c
Hiroki Noda <kubo39@gmail.com>
2019-03-03 05:28:10 +0900

    Tidy fix

--- Pow.thy
+++ Pow.thy
@@ -8,8 +8,6 @@ fun pow :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
   "pow _ 0 = 1"
   | "pow a (Suc n') = a * pow a n'"
 
-value "pow 0 0"
-
 lemma test_pow: "pow x 0 = 1" by auto
 
 theorem pow_0: "\<forall>x :: nat. pow x 0 = 1" by simp
